34be03401bfa5928bcda730b498a767a2d56711e
Moyo Olojede <mo11552@icloud.com>
2023-06-09 14:54:11 -0400

    morgan middleware added, express.static added and get route added

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const express = require('express');
 const app = express();
+
+let topMovies = [
+  {
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien'
+  },
+  {
+    title: 'Twilight',
+    author: 'Stephanie Meyer'
+  }
+];
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('My top ten movies');
+});
+
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+// express.static
+app.use(express.static('public'));
