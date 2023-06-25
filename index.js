2b5f7e7dab05687aff1d060d933aefb8423dc9cf
Moyo Olojede <mo11552@icloud.com>
2023-06-24 21:00:11 -0400

    update, post and delete user endpoints added as well as imageURL endpoint

--- index.js
+++ index.js
@@ -163,8 +163,6 @@ let topMovies = [
 ];
 
 // CREATE
-
-// POSTMAN
 app.post('/users', (req, res) => {
 	const newUser = req.body;
 
@@ -176,14 +174,72 @@ app.post('/users', (req, res) => {
 		res.status(400).send('users need names')
 	}
 })
-// Web browser
+
+// UPDATE
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user =users.find( user => user.id == id );
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+      res.status(400).send('no such user')
+  }
+})
+
+// POST
+app.post('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+ 
+  let user =users.find( user => user.id == id );
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);;  
+  } else {
+      res.status(400).send('no such user')
+  }
+})
+
+// DELETE
+app.delete('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+ 
+  let user =users.find( user => user.id == id );
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
+    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);;  
+  } else {
+      res.status(400).send('no such user')
+  }
+})
+
+// DELETE
+app.delete('/users/:id/', (req, res) => {
+  const { id } = req.params;
+ 
+  let user =users.find( user => user.id == id );
+
+  if (user) {
+    users = users.filter(user => user.id != id);
+    res.status(200).send(`user ${id} has been deleted`);  
+  } else {
+      res.status(400).send('no such user')
+  }
+})
+
+// READ
+
 app.get('/users', (req, res) => {
 
 res.status(200).json(users);
 
 })
 
-// READ
 app.get('/movies', (req, res) => {
   res.status(200).json(topMovies);
 })
@@ -221,6 +277,18 @@ app.get('/movies/director/:directorName', (req, res) => {
   	res.status(200).json(director);
   } else {
   	res.status(400).send("no such director")
+  }
+})
+
+// READ
+app.get('/movies/director/:directorImage', (req, res) => {
+  const { directorImage } = req.params;
+  const directorPic = topMovies.find( movie => movie.director.imageURL === directorImage ).director;
+
+  if (directorPic) {
+    res.status(200).json(directorPic);
+  } else {
+    res.status(400).send("no picture")
   }
 })
 
